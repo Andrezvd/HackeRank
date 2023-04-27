7021dc9e81c38ad6b505eea432cb317d0255bcc6
Andrezvd <andres2211556@correo.uis.edu.co>
2023-04-26 22:50:39 -0500

    ALGORITMO DE PARSING CON ETIQUETAS C++

--- MergeSort/MergeSort.cpp
+++ MergeSort/MergeSort.cpp
@@ -1,12 +1,81 @@
 // MergeSort.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
+#include <algorithm>
+#include <map>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+vector<string> tag_stack;
+map<string, string> attrs;
 
+
+void insert_attr(string& name, string& val) {
+    string full;
+    for (string& str : tag_stack)
+        full += str + ".";
+    full.pop_back();
+    full += "~" + name;
+    attrs[full] = val;
+
+}
+
+int main() {
+    int n, q;
+    std::cout << "ingrese los dos valores" << std::endl;
+    cin >> n >> q;
+
+    for (int i = 0; i < n; ++i) {
+        char c; cin >> c;
+        if (cin.peek() == '/') { // close
+            string cn; cin >> cn;
+            tag_stack.pop_back();
+        }
+        else { //open'
+            string name;
+            cin >> name;
+            if (name.back() == '>') { //no attrs
+                name.pop_back();
+                tag_stack.push_back(name);
+            }
+            else {
+                tag_stack.push_back(name);
+
+                for (;;) {
+                    string attr_name, attr_val, eq;
+                    cin >> attr_name >> eq >> attr_val;
+                    if (attr_val.back() == '>') { //last attr
+                        attr_val.pop_back();
+                        attr_val.pop_back();
+                        attr_val = attr_val.substr(1);
+                        insert_attr(attr_name, attr_val);
+                        break;
+                    }
+                    else {
+                        attr_val.pop_back();
+                        attr_val = attr_val.substr(1);
+                        insert_attr(attr_name, attr_val);
+                    }
+                }
+            }
+
+        }
+    }
+
+    for (int i = 0; i < q; ++i) {
+        string quer;
+        cin >> quer;
+        if (attrs.find(quer) != attrs.end())
+            cout << attrs[quer] << endl;
+        else
+            cout << "Not Found!" << endl;
+    }
+
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
